a91d73a44c4ef3b081f66ba7290feb7b9ecfcf0a
Tjiho Lun <tom.darboux@eisti.fr>
2018-04-28 14:07:52 +0200

    add room on map

--- static/create-map.js
+++ static/create-map.js
@@ -1,9 +1,53 @@
-var map = L.map('content').setView([55.3673174, 10.4305942], 20);
+function drawRoom(data,map)
+{
+    var myStyle = {
+        "color": "#000000",
+        "weight": 1 ,
+        "opacity": 0.65
+    };
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    L.geoJSON(
+        data.features,
+        {
+            filter: function(feature, layer) {
+                return feature.properties.ZLevel == "0";
+            },
+            style: myStyle
+        }
+    ).addTo(map);
+}
 
-L.marker([55.3673174, 10.4310942]).addTo(map) // [55.3673174, 10.4310942] , [55.3673174, 10.4305942]
-    .bindPopup('<a href="https://qwant.com"/>qwant.com</a>')
-    .openPopup();
+function ajaxRoom(map)
+{
+    var xhr = xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === 4)
+        {
+            drawRoom(JSON.parse(xhr.responseText),map)
+        }
+    }
+    xhr.open('GET', '/static/ou44_geometry.geojson', true)
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send();
+}
+
+function createMap()
+{
+    var map = L.map('content').setView([55.3673174, 10.4305942], 20);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([55.3673174, 10.4310942]).addTo(map)
+        .bindPopup('<a href="https://qwant.com"/>qwant.com</a>')
+        .openPopup();
+
+    return map
+}
+
+
+map = createMap()
+ajaxRoom(map)
--- static/ws.js
+++ static/ws.js
@@ -1,6 +1,7 @@
-var socket = io();
 
+var socket = io();
 socket.on('logs', function(msg){
     logs = document.querySelector('#list-logs')
     logs.innerHTML += '<li>'+msg+'</li>'
 });
+
